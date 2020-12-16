93d39d1becda9d6700edd2059b98f7f94ed523a0
sungillee90 <sungil.lee90@gmail.com>
2020-12-16 22:02:36 +0000

    first ruby commit

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
-address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-p address
-p address[3]
-new_address = address.reverse!
-p new_address
+# address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# p address
+# p address[3]
+# new_address = address.reverse!
+# p new_address
+
+# put String == puts
+puts "Hello World"
+
+greeting = "hello world"
+p greeting
