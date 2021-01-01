7f1ec38f97b4c1b913dc1193586f5572a3d5ecab
sungillee90 <sungil.lee90@gmail.com>
2021-01-01 20:02:12 +0000

    Working with numbers

--- main.rb
+++ main.rb
@@ -25,9 +25,9 @@
 #  String interpolation
 # puts "My first name is #{first_name}, and my last name is #{last_name}"
 
-puts "What is your first name?"
-first_name = gets.chomp
-puts "Thank you, you said your first name is #{first_name}"
+# puts "What is your first name?"
+# first_name = gets.chomp
+# puts "Thank you, you said your first name is #{first_name}"
 
 # puts "Enter a number to multiply by 2"
 # input = gets.chomp
@@ -44,11 +44,22 @@ puts "Thank you, you said your first name is #{first_name}"
 # puts "Your name has #{full_name.length - 1} characters in it"
 
 
-puts "Enter your age in 2020"
-age = gets.chomp
-puts "This is your new age in 2021!"
-puts age.to_i
-age_new = age.to_i + 1
+# puts "Enter your age in 2020"
+# age = gets.chomp
+# puts "This is your new age in 2021!"
+# puts age.to_i
+# age_new = age.to_i + 1
 
-puts "Your age in 2020 is #{age}"
-puts "Your new age in 2021 is #{age_new}"
+# puts "Your age in 2020 is #{age}"
+# puts "Your new age in 2021 is #{age_new}"
+
+puts 1 + 2
+puts 10 / 4  # 2
+# automatically change to float
+puts 10.0 /4 # 2.5
+
+# Has similar with Java
+puts "5" * 2
+
+puts 20.times{print "-"}
+puts "-" * 20
